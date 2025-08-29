19748ddff0af28d14e2c5efd105ac2d0ea782aad
Bruno Lima <bru.no@outlook.com.br>
2025-08-28 22:46:37 -0300

    🧪 test(main): amplia testes para cadastro e listagem de itens do cardápio
    - Adiciona 5 itens de cardápio com nome e preço;
    - Lista clientes e itens no console com totais;
    - Melhora a legibilidade da saída com separadores visuais.

--- src/main/java/com/fooddelivery/Main.java
+++ src/main/java/com/fooddelivery/Main.java
@@ -1,36 +1,50 @@
 package com.fooddelivery;
 
-import java.util.List;
-
 import com.fooddelivery.model.Cliente;
+import com.fooddelivery.model.ItemCardapio;
+import java.util.List;
+import java.math.BigDecimal;
 
 public class Main {
     public static void main(String args[]) {
 
-        System.out.println("\n\nCadastrar Clientes:\n\n");
+        System.out.println("-------------------------------");
+        System.out.println("Cadastrando 5 clientes:");
 
         Cliente c1 = new Cliente("Bruno", "77 9 8873 4773");
-        System.out.println("Cliente: " + c1.getNome() + " | Telefone: " + c1.getTelefone());
-
         Cliente c2 = new Cliente("Claudio", "77 9 8815 1047");
-
-        System.out.println("Cliente: " + c2.getNome() + " | Telefone: " + c2.getTelefone());
         Cliente c3 = new Cliente("Gabriel", "77 9 1234 5678");
-
-        System.out.println("Cliente: " + c3.getNome() + " | Telefone: " + c3.getTelefone());
         Cliente c4 = new Cliente("Kéven", "77 9 8765 4321");
-
-        System.out.println("Cliente: " + c4.getNome() + " | Telefone: " + c4.getTelefone());
-
         Cliente c5 = new Cliente("Artur", "77 9 9999 9999");
-        System.out.println("Cliente: " + c5.getNome() + " | Telefone: " + c5.getTelefone());
 
         List<Cliente> clientes = Cliente.getListaClientes();
 
-        System.out.println("\n\nQuantidade de Clientes cadastrados: " + clientes.size() + "\n\n");
+        System.out.println("--------------------------------------------------------");
+        System.out.println("Lista de clientes cadastrados: (Total = " + clientes.size() + ")");
+        System.out.println("--------------------------------------------------------");
 
         for (Cliente cliente : clientes) {
-            System.out.println("Nome: " + cliente.getNome() + " | Telefone: " + cliente.getTelefone());
+            System.out.println("| ID: " + cliente.getId() + " | Nome: " + cliente.getNome() + " | Telefone: "
+                    + cliente.getTelefone());
+        }
+
+        System.out.println("\n\n-------------------------------");
+        System.out.println("Cadastrando 5 itens no cardápio:");
+
+        ItemCardapio i1 = new ItemCardapio("X-Burguer", new BigDecimal(42.00));
+        ItemCardapio i2 = new ItemCardapio("Cola-Cola 1L", new BigDecimal(10.00));
+        ItemCardapio i3 = new ItemCardapio("Batata Frita M", new BigDecimal(18.00));
+        ItemCardapio i4 = new ItemCardapio("Fini", new BigDecimal(7.999));
+        ItemCardapio i5 = new ItemCardapio("Picolé", new BigDecimal(3.996));
+
+        List<ItemCardapio> itensCardapio = ItemCardapio.getListaCardapio();
+
+        System.out.println("--------------------------------------------------------");
+        System.out.println("Lista de itens cadastrados no cardápio: (Total = " + itensCardapio.size() + ")");
+        System.out.println("--------------------------------------------------------");
+
+        for (ItemCardapio item : itensCardapio) {
+            System.out.println("| ID: " + item.getId() + " | Nome: " + item.getNome() + " | Preço: " + item.getPreco());
         }
     }
 }
