ac4b8fa291045a241de25ffbb8661cf9eee2192c
Bruno Lima <bru.no@outlook.com.br>
2025-08-30 12:29:12 -0300

    🧪 test(main): adiciona menu de opções e tela inicial
    Atualiza a classe `Main` para testes da tela inicial e menu de navegação.
    
    - Configura terminal para UTF-8 para suportar caracteres especiais;
    - Exibe ASCII Art da tela inicial com `TelaInicial.exibir()`;
    - Implementa switch com opções simuladas para cardápio, clientes, pedidos e relatórios;
    - Placeholder de mensagens para cada funcionalidade.

--- src/main/java/com/fooddelivery/Main.java
+++ src/main/java/com/fooddelivery/Main.java
@@ -4,98 +4,68 @@
 
 package com.fooddelivery;
 
-import java.math.BigDecimal;
-import com.fooddelivery.model.Cliente;
-import com.fooddelivery.model.ItemCardapio;
-import com.fooddelivery.model.ItemPedido;
-import com.fooddelivery.service.ClienteService;
-import com.fooddelivery.service.PedidoService;
-import com.fooddelivery.service.CardapioService;
-import com.fooddelivery.model.Pedido;
-import com.fooddelivery.model.Pedido.StatusPedido;
+import com.fooddelivery.view.TelaInicial;
 
 public class Main {
-
     public static void main(String args[]) {
 
-        ClienteService clientService = new ClienteService();
-        CardapioService cardapioService = new CardapioService();
-        PedidoService pedidoService = new PedidoService();
-
-        System.out.println("-------------------------------");
-        System.out.println("Cadastrando 5 clientes:");
-
-        clientService.cadastrarCliente("Bruno", "77 9 8873 4773");
-        clientService.cadastrarCliente("Claudio", "77 9 8815 1047");
-        clientService.cadastrarCliente("Gabriel", "77 9 1234 5678");
-        clientService.cadastrarCliente("Kéven", "77 9 8765 4321");
-        clientService.cadastrarCliente("Artur", "77 9 9999 9999");
-
-        System.out.println("--------------------------------------------------------");
-        System.out.println("Lista de clientes cadastrados: (Total = " + clientService.listarClientes().size() + ")");
-        System.out.println("--------------------------------------------------------");
-
-        for (Cliente cliente : clientService.listarClientes()) {
-            System.out.println("| ID: " + cliente.getId() + " | Nome: " + cliente.getNome() + " | Telefone: "
-                    + cliente.getTelefone());
+        // Tenta configurar o terminal para usar caracteres UTF-8:
+        try {
+            System.setOut(new java.io.PrintStream(System.out, true, "UTF-8"));
+        } catch (java.io.UnsupportedEncodingException e) {
+            // Se falhar, ignora configuração e continua execução da aplicação
+            System.err.println("Erro ao configurar terminal para utilizar UTF-8: " + e.getMessage());
         }
-        System.out.println("--------------------------------------------------------");
 
-        System.out.println("\n\n-------------------------------");
-        System.out.println("Cadastrando 5 itens no cardápio:");
+        TelaInicial.exibir();
 
-        cardapioService.cadastrarItemCardapio("X-Burguer", new BigDecimal(42.00));
-        cardapioService.cadastrarItemCardapio("Cola-Cola 1L", new BigDecimal(10.00));
-        cardapioService.cadastrarItemCardapio("Batata Frita M", new BigDecimal(18.00));
-        cardapioService.cadastrarItemCardapio("Fini", new BigDecimal(7.999));
-        cardapioService.cadastrarItemCardapio("Picolé", new BigDecimal(3.996));
+        int opcaoEscolhida = 0;
 
-        System.out.println("--------------------------------------------------------");
-        System.out.println(
-                "Lista de itens cadastrados no cardápio: (Total = " + cardapioService.listarCardapio().size() + ")");
-        System.out.println("--------------------------------------------------------");
+        switch (opcaoEscolhida) {
+            // GERENCIAMENTO DE CARDÁPIO
+            case 1:
+                System.out.println("Cadastrando novo item no cardápio...");
+                break;
+            case 2:
+                System.out.println("Listando itens do cardápio...");
+                break;
 
-        for (ItemCardapio itemCardapio : cardapioService.listarCardapio()) {
-            System.out.println("| ID: " + itemCardapio.getId() + " | Item: " + itemCardapio.getNome() + " | Preço: "
-                    + itemCardapio.getPreco());
+            // GERENCIAMENTO DE CLIENTES
+            case 3:
+                System.out.println("Cadastrando novo cliente...");
+                break;
+            case 4:
+                System.out.println("Listando clientes cadastrados...");
+                break;
+
+            // GERENCIAMENTO DE PEDIDOS
+            case 5:
+                System.out.println("Registrando novo pedido...");
+                break;
+            case 6:
+                System.out.println("Atualizando status do pedido...");
+                break;
+            case 7:
+                System.out.println("Consultando pedidos por status...");
+                break;
+
+            // RELATÓRIOS
+            case 8:
+                System.out.println("Gerando relatório simplificado...");
+                break;
+            case 9:
+                System.out.println("Gerando relatório detalhado...");
+                break;
+
+            // ENCERRAR SISTEMA
+            case 10:
+                System.out.println("Encerrando sistema...");
+                break;
+
+            default:
+                System.out.println("Opção inválida");
+                break;
         }
-        System.out.println("--------------------------------------------------------");
 
-        System.out.println("\n\n-------------------------------");
-        System.out.println("Realizando 6 pedidos:");
-
-        pedidoService.criarPedido(StatusPedido.ACEITO);
-        pedidoService.criarPedido(StatusPedido.PREPARANDO);
-        pedidoService.criarPedido(StatusPedido.FEITO);
-        pedidoService.criarPedido(StatusPedido.AGUARDANDO_ENTREGADOR);
-        pedidoService.criarPedido(StatusPedido.SAIU_PARA_ENTREGA);
-        pedidoService.criarPedido(StatusPedido.ENTREGUE);
-
-        System.out.println("--------------------------------------------------------");
-        System.out.println("Lista de pedidos efetuados: (Total = " + pedidoService.listarPedidos().size() + ")");
-        System.out.println("--------------------------------------------------------");
-
-        for (Pedido pedido : pedidoService.listarPedidos()) {
-            System.out.println("| ID: " + pedido.getId() + " | Data: " + pedido.getData() + " | Status: "
-                    + pedido.getStatus());
-        }
-        System.out.println("--------------------------------------------------------");
-
-        System.out.println("\n\n-------------------------------");
-        System.out.println("Cadastrando 3 itens pedidos:");
-
-        ItemPedido i1 = new ItemPedido(20, new BigDecimal(10.398));
-        ItemPedido i2 = new ItemPedido(10, new BigDecimal(5.589));
-        ItemPedido i3 = new ItemPedido(5, new BigDecimal(2.542));
-
-        System.out.println("--------------------------------------------------------");
-        System.out.println("Lista de itens pedidos:");
-        System.out.println("--------------------------------------------------------");
-
-        System.out.println("| Quantidade: " + i1.getQuantidade() + " | Preço Unitário: " + i1.getPrecoUnit());
-        System.out.println("| Quantidade: " + i2.getQuantidade() + " | Preço Unitário: " + i2.getPrecoUnit());
-        System.out.println("| Quantidade: " + i3.getQuantidade() + " | Preço Unitário: " + i3.getPrecoUnit());
-
-        System.out.println("--------------------------------------------------------");
     }
 }
