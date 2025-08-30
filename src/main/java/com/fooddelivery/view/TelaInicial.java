9d7771974246bd7a91597351cf5385656c33a576
Bruno Lima <bru.no@outlook.com.br>
2025-08-30 12:18:15 -0300

    ✨ feat(view): adiciona tela inicial em ASCII Art para o sistema
    - Cria a classe `TelaInicial` com método `exibir()`;
    - Exibe banner do sistema e menu de opções no console;
    - Inclui instruções para interação do usuário.

---
+++ src/main/java/com/fooddelivery/view/TelaInicial.java
@@ -0,0 +1,45 @@
+package com.fooddelivery.view;
+
+public class TelaInicial {
+    public static void exibir() {
+        System.out.println(
+                        "┌─────────────────────────────────────────────────────────────────┐\r\n" + //
+                        "│                                                                 │\r\n" + //
+                        "│          ████████╗ ██╗    █████╗     ██╗     ██╗   ██╗          │\r\n" + //
+                        "│          ╚══██╔══╝ ██║  ║██╔══██╗    ██║     ██║   ██║          │\r\n" + //
+                        "│             ██║    ██║  ║███████║    ██║     ██║   ██║          │\r\n" + //
+                        "│             ██║    ██║  ║██╔══██║    ██║     ██║   ██║          │\r\n" + //
+                        "│             ██║    ██║  ║██║  ██║    ███████╗╚██████╔╝          │\r\n" + //
+                        "│             ╚═╝    ╚═╝  ╚══╝  ╚═╝    ╚══════╝ ╚═════╝           │\r\n" + //
+                        "│                                                                 │\r\n" + //
+                        "│  Sistema de Delivery - Versão 1.0                               │\r\n" + //
+                        "│  =============================================================  │\r\n" + //
+                        "│                                                                 │\r\n" + //
+                        "│  Funções disponíveis:                                           │\r\n" + //
+                        "│                                                                 │\r\n" + //
+                        "│  GERENCIAMENTO DE CARDÁPIO:                                     │\r\n" + //
+                        "│    1 - Cadastrar Novo Item                                      │\r\n" + //
+                        "│    2 - Listar Itens                                             │\r\n" + //
+                        "│                                                                 │\r\n" + //
+                        "│  GERENCIAMENTO DE CLIENTES:                                     │\r\n" + //
+                        "│    3 - Cadastrar Novo Cliente                                   │\r\n" + //
+                        "│    4 - Listar Clientes                                          |\r\n" + //
+                        "│                                                                 │\r\n" + //
+                        "│  GERENCIAMENTO DE PEDIDOS:                                      │\r\n" + //
+                        "│    5 - Registrar Novo Pedido                                    │\r\n" + //
+                        "│    6 - Atualizar Status Pedido                                  │\r\n" + //
+                        "│    7 - Consultar Pedidos por Status                             │\r\n" + //
+                        "│                                                                 │\r\n" + //
+                        "│  RELATÓRIOS:                                                    │\r\n" + //
+                        "│    8 - Relatório Simplificado                                   │\r\n" + //
+                        "│    9 - Relatório Detalhado                                      │\r\n" + //
+                        "│                                                                 │\r\n" + //
+                        "│  ENCERRAR SISTEMA:                                              │\r\n" + //
+                        "│   10 - Sair                                                     │\r\n" + //
+                        "│                                                                 │\r\n" + //
+                        "│                                                                 │\r\n" + //
+                        "│ Insira o número da função desejada e tecle Enter:               │\r\n" + //
+                        "│                                                                 │\r\n" + //
+                        "└─────────────────────────────────────────────────────────────────┘");
+    }
+}
